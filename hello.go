6ada1c492686fbf26efd66a071ee5bf86316dc58
Matthew R Kasun <mkasun@nusak.ca>
2023-05-30 05:26:30 -0400

    use netmaker repo

--- hello.go
+++ hello.go
@@ -1,9 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/gravitl/netmaker/models"
+)
 
 func main() {
+	user := models.User{
+		UserName: "matt",
+		Password: "kasun",
+		IsAdmin:  true,
+	}
 	fmt.Println("hello world!")
 	fmt.Println("another message")
 	fmt.Println("third message")
+	fmt.Println(user)
 }
