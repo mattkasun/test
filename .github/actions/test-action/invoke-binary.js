40db8963ee41c21b6d6d9af01efc3ea5373b2dd6
Matthew R Kasun <mkasun@nusak.ca>
2023-10-04 13:20:55 -0400

    try passing arg to call go code

--- .github/actions/test-action/invoke-binary.js
+++ .github/actions/test-action/invoke-binary.js
@@ -7,5 +7,5 @@ function chooseBinary() {
 var  child_process   = require('node:child_process');
 const binary = chooseBinary();
 const mainScript = `${__dirname}/${binary}`;
-var spawnSyncReturns = child_process.spawnSync(mainScript, { stdio: 'inherit' });
+var spawnSyncReturns = child_process.spawnSync(mainScript clean, { stdio: 'inherit' });
 console.log('stdout:\n'+spawnSyncReturns.stdout)
