a98832f539392530f6ca14de77244ca97399b914
Matthew R Kasun <mkasun@nusak.ca>
2023-10-04 13:46:12 -0400

    js imports

--- .github/actions/test-action/invoke-binary.js
+++ .github/actions/test-action/invoke-binary.js
@@ -3,15 +3,15 @@ function chooseBinary() {
         return `main-linux-amd64-v1.0.0`
     // ...
 }
-const core = require('@actions/core');
+import { getInput, SetFailed } from '@actions/core';
 try {
 var  child_process   = require('node:child_process');
 const binary = chooseBinary();
 const mainScript = `${__dirname}/${binary}`;
-const args = core.getInput('cmd-args');
+const args = getInput('cmd-args');
 var spawnSyncReturns = child_process.spawnSync(mainScript, args, { stdio: 'inherit' });
 console.log('stdout:\n'+spawnSyncReturns.stdout);
 } catch (error) {
-    core.SetFailed(error.message);
+    SetFailed(error.message);
     
 }
