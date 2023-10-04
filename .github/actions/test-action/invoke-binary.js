32d7c825c3525ea46d9a3a2f457dfa8b05cb437c
Matthew R Kasun <mkasun@nusak.ca>
2023-10-04 13:38:15 -0400

    pass args
    Signed-off-by: Matthew R Kasun <mkasun@nusak.ca>

--- .github/actions/test-action/invoke-binary.js
+++ .github/actions/test-action/invoke-binary.js
@@ -3,9 +3,15 @@ function chooseBinary() {
         return `main-linux-amd64-v1.0.0`
     // ...
 }
-
+const core = require('@actions/core');
+try {
 var  child_process   = require('node:child_process');
 const binary = chooseBinary();
 const mainScript = `${__dirname}/${binary}`;
-var spawnSyncReturns = child_process.spawnSync(mainScript, clean, { stdio: 'inherit' });
-console.log('stdout:\n'+spawnSyncReturns.stdout)
+const args = core.getInput('cmd-args');
+var spawnSyncReturns = child_process.spawnSync(mainScript, args, { stdio: 'inherit' });
+console.log('stdout:\n'+spawnSyncReturns.stdout);
+} catch (error) {
+    core.SetFailed(error.message);
+    
+}
