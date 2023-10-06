8a87c805050db1982dc7b63fe50cf6f2c8ca1cb7
Matthew R Kasun <mkasun@nusak.ca>
2023-10-06 09:23:05 -0400

    pass args correctly

--- .github/actions/test-action/invoke-binary.js
+++ .github/actions/test-action/invoke-binary.js
@@ -3,13 +3,13 @@ function chooseBinary() {
         return `main-linux-amd64-v1.0.0`
     // ...
 }
-import core from '@actions/core';
-import child_process from '@node/child_process'
+const  child_process   = require('node:child_process');
+const core = require('@actions/core')
 try {
 const binary = chooseBinary();
 const mainScript = `${__dirname}/${binary}`;
-const args = getInput('cmd-args');
-var spawnSyncReturns = child_process.spawnSync(mainScript, args, { stdio: 'inherit' });
+const args = core.getInput('cmd-args');
+var spawnSyncReturns = child_process.spawnSync(mainScript, [args], { stdio: 'inherit' });
 console.log('stdout:\n'+spawnSyncReturns.stdout);
 } catch (error) {
     core.setFailed(error.message);
