14b0ee2905f8e1ebe20c452949eb8bd4f7809217
Matthew R Kasun <mkasun@nusak.ca>
2023-10-06 09:29:49 -0400

    logging

--- .github/actions/test-action/invoke-binary.js
+++ .github/actions/test-action/invoke-binary.js
@@ -8,7 +8,9 @@ const core = require('@actions/core')
 try {
 const binary = chooseBinary();
 const mainScript = `${__dirname}/${binary}`;
+console.log("bin", mainScript)
 const args = core.getInput('cmd-args');
+console.log("args", args)
 var spawnSyncReturns = child_process.spawnSync(mainScript, [args], { stdio: 'inherit' });
 console.log('stdout:\n'+spawnSyncReturns.stdout);
 } catch (error) {
